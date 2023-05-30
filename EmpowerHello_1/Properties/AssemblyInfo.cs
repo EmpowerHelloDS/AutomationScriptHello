1f455cd40bb731ffdd5caf79fee3bc4d662516ae
dev-diogos <134947916+dev-diogos@users.noreply.github.com>
2023-05-30 15:55:31 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerHello_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHello_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHello_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("BEF3786E-CAC9-44D2-9420-0A3C2718F972")]
+[assembly: AssemblyVersion("1.0.0.0")]
